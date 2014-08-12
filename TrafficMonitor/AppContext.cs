90484d0aecabbe2419342de62d35711ac6ee7de5
Marco <m.gester@gmx.net>
2014-08-12 14:25:24 +0200

    Addes ByteToGbFormatter for display of bytes, not properly implementd yet

---
+++ TrafficMonitor/AppContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Collections.ObjectModel;
+
+namespace TrafficMonitor
+{
+    public sealed class AppContext
+    {
+        public AppContext()
+        {
+            Adapters = new ObservableCollection<NetworkAdapterContext>();
+        }
+
+        public ObservableCollection<NetworkAdapterContext> Adapters { get; set; }
+        public NetworkAdapterContext SelectedAdapter { get; set; }
+
+
+
+        internal void Start()
+        {
+            var interfaceTemp = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var interfs in interfaceTemp)
+            {
+                Adapters.Add(new NetworkAdapterContext(interfs));
+            }
+        }
+    }
+}
---
+++ TrafficMonitor/ByteToGbFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TrafficMonitor
+{
+    [ValueConversion(typeof(String),typeof(String))]
+
+    public sealed class ByteToGbFormatter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var s = (String)value;
+            long temp = long.Parse(s);
+
+            if (temp >= 1024 && temp < 1048576 )
+            {
+                return s = (temp / 1024).ToString() + (" KB");
+            }
+            if (temp >= 1048576 && temp < 1073741824)
+            {
+                return s = (temp / 1048576).ToString() + (" MB");
+            }
+            if (temp >= 1073741824 )
+            {
+                return s = (temp / 1073741824).ToString() + ("GB");
+            }
+            else
+            {
+                return s;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
